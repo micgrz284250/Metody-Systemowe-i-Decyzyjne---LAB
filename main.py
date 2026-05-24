b3ff988645af2f015aa5f080bc6a36b2f21d4e69
Stanislaw <s.podrecki@gmail.com>
2026-05-24 13:58:53 +0200

    refaktoryzacja, testy i doprawadzanie do działania simmulated annealingu

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from ant_optimization.pheromones import Pheromones
 from test import show_graph, parse_problem_data_text_to_nx_graph, evaluate
 
+
 def main():
     colors_key = [i for i in range(1, 139)]
     colors_values = [i for i in range(1, 139)]
     colors = dict(zip(colors_key, colors_values))
-    graph_file = 'instances/anna.col'
+    graph_file = "instances/anna.col"
     problem_graph = parse_problem_data_text_to_nx_graph(graph_file)
     show_graph(problem_graph, colors)
     print(evaluate(problem_graph, colors))
     ph = Pheromones(problem_graph, colors)
-    print('hello')
+    print("hello")
+
 
 if __name__ == "__main__":
     main()
