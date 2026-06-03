c6cf8e206b09b37cee595a501c14b91029ce140d
Michał Grzebielec <284250@student.pwr.edu.pl>
2026-06-03 00:50:11 -0700

    ant done

--- ant_optimization/ant.py
+++ ant_optimization/ant.py
@@ -1,42 +1,39 @@
 import random
 
-from networkx.classes import graph
-
 from ant_optimization.pheromones import Pheromones
 
 
 class Ant:
-    def __init__(self, ant_id, graph):
+    def __init__(self, ant_id, graph, heuristic_weight=4.0, pheromone_weight=2.0):
         self.ant_id = ant_id
         self.graph = graph
         self.pheromones = Pheromones(graph)
-        self.choosable = set(graph.nodes())
+        self.choosable = set()
         self.blocked = set()
-        self.colors = {node: None for node in graph.nodes()}
+        self.colored = 0
+        self.colors = {node: None for node in self.graph.nodes}
+        self.heuristic_weight = heuristic_weight
+        self.pheromone_weight = pheromone_weight
 
+    def __str__(self):
+        return f'Ant {self.ant_id}: colored {self.colored} nodes from all {self.graph.number_of_nodes} nodes'
 
     def run(self):
         curr_colors = 1
 
         #iterujemy, dopóki są jeszcze dostępne node w zbiorze wybieralnych
-        while self.choosable:
-            # resetujemy zbiory wybieralnych i zablokowanych
-            self.reset()
-
-            # pobieramy startowy node dla zbioru
-            start_node = self.get_start_point()
-
-            # usuwamy sąsiadów startowego node ze zbioru wybieralnych
-            self.remove_neighbors(start_node)
-
-
-
-            # koniec tworzenia danego zbioru, zwiększamy kolor
+        while self.colored != self.graph.number_of_nodes():
+            self.create_color_subset(curr_colors)
             curr_colors += 1
 
 
+    def reset(self):
+        self.choosable = set(node for node in self.colors.keys() if self.colors[node] is not None)
+        self.blocked = set()
+
     def get_start_point(self):
         return random.choice(list(self.choosable))
+
 
     def remove_neighbors(self, node):
         if node in self.choosable:
@@ -52,11 +49,56 @@ class Ant:
             if neighbor not in self.blocked:
                 self.blocked.add(neighbor)
 
-    def reset(self):
-        self.choosable = set()
-        self.blocked = set()
-        for node in self.graph.nodes():
-            if node in self.colors.keys():
-                self.choosable.add(node)
-            else:
-                self.blocked.add(node)
+
+    def count_blocked_neighbors(self, node):
+        count = 0
+        for neighbor in self.graph.neighbors(node):
+            if neighbor in self.blocked:
+                count += 1
+        return count
+
+
+    def read_pheromones(self, node, color_subset):
+        pheromone = 0.0
+        for colored_node in color_subset:
+            pheromone += self.pheromones.get_pheromone(node, colored_node)
+        pheromone /= len(color_subset)
+        return pheromone
+
+
+    def calculate_probability(self, node, color_subset):
+        pheromone = self.read_pheromones(node, color_subset)
+        blocked_neighbors = self.count_blocked_neighbors(node)
+        return pheromone**self.pheromone_weight + blocked_neighbors**self.pheromone_weight
+
+
+    def create_color_subset(self, color):
+        # definiujemy sobie podzbiór node, które będą miały ten sam kolor
+        color_subset = set()
+
+        # resetujemy zbiory wybieralnych i zablokowanych
+        self.reset()
+
+        # pobieramy startowy node dla zbioru
+        start_node = self.get_start_point()
+        self.colored += 1
+        color_subset.add(start_node)
+        self.colors[start_node] = color
+
+        # usuwamy sąsiadów startowego node ze zbioru wybieralnych
+        self.remove_neighbors(start_node)
+
+        while self.choosable:
+            # obliczamy prawdopodobieństwo dla każdego node ze zbioru wybieralnych
+            probabilities = {node: self.calculate_probability(node, color_subset) for node in self.choosable}
+
+            # na podstawie prawdopodobieństwa losujemy node do pokolorowania
+            total_probability = sum(probabilities.values())
+            if total_probability == 0:
+                probabilities = {node: 1.0 / len(self.choosable) for node in self.choosable}
+            selected_node = random.choices(list(probabilities.keys()), weights=list(probabilities.values()), k=1)[0]
+
+            self.colored += 1
+            color_subset.add(selected_node)
+            self.colors[selected_node] = color
+            self.remove_neighbors(selected_node)
