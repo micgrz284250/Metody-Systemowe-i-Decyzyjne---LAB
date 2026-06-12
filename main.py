89d85c540c4af9b8beb4ef0d0cf2ee5ae9b0cd38
Michał Grzebielec <284250@student.pwr.edu.pl>
2026-06-12 22:34:53 +0200

    some enters

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from test import parse_problem_data_text_to_nx_graph, show_graph
 
 
 def main():
-    graph_file = 'instances/le450_15b.col'
+    graph_file = 'instances/school1_nsh.col'
     problem_graph = parse_problem_data_text_to_nx_graph(graph_file)
-    colors, score = colony.optimize(problem_graph, num_ants=10, num_iterations=10, heuristic_weight=4.0)
+
+    print(len(problem_graph.nodes))
+
+    colors, score = colony.optimize(problem_graph, num_ants=500, num_iterations=1000, heuristic_weight=4.0, threads=16)
+
     print(f'Found solution: {colors}')
     print(f'Score: {score}')
+
     show_graph(problem_graph, colors)
 
 if __name__ == "__main__":
