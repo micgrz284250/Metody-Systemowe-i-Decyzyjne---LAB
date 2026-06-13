da9769dee5cbe7587db4c7f5a85228d18e9ebb9c
Michał Grzebielec <284250@student.pwr.edu.pl>
2026-06-13 02:11:46 +0200

    added set of specific graphs to compare algorithms

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from test import parse_problem_data_text_to_nx_graph, show_graph, evaluate
 
 
 def main():
-    graph_file = 'instances/school1_nsh.col'
+    graph_file = 'test_graphs/cliques_8x12_bridges.col'
     problem_graph = parse_problem_data_text_to_nx_graph(graph_file)
 
     print(len(problem_graph.nodes))
 
-    colors, score = colony.optimize(problem_graph, num_ants=100, num_iterations=100, heuristic_weight=2.0, pheromone_weight=3.0, evaporate_value=0.5, threads=16)
+    colors, score = colony.optimize(problem_graph, num_ants=50, num_iterations=100, heuristic_weight=4.0, pheromone_weight=2.0, evaporate_value=0.5, threads=16)
 
     print(f'Found solution: {colors}')
     print(f'Score: {score}')
     print(f'Evaluated {evaluate(problem_graph, colors)}')
 
-    # show_graph(problem_graph, colors)
+    show_graph(problem_graph, colors)
 
 if __name__ == "__main__":
     main()
